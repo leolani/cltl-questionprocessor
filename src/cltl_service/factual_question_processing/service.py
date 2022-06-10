be9f7259134f7f8ecaee53ad9faab38300dd2855
Thomas Baier <tbaier@math.bme.hu>
2022-06-10 10:20:21 +0200

    Adjust TextSignalEvent creation

--- src/cltl_service/factual_question_processing/service.py
+++ src/cltl_service/factual_question_processing/service.py
@@ -71,4 +71,4 @@ class FactualResponderService:
         scenario_id = self._emissor_data.get_current_scenario_id()
         signal = TextSignal.for_scenario(scenario_id, timestamp_now(), timestamp_now(), None, response)
 
-        return TextSignalEvent.create(signal)
+        return TextSignalEvent.for_agent(signal)
